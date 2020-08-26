b93702d0b56ff845746a2e0e74b0a8ca8c47fe35
Kristjan <radiojustchill@gmail.com>
2020-08-26 18:23:14 +0300

    Updated Pet, Owners and PetType with Entity mapping. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,11 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Owner extends Person {
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
     private String address;
     private String city;
     private String telephone;
