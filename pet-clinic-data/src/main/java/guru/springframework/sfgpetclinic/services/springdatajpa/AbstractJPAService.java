f58f8dfbdc7d3c9a342100524bf07d57329f1aa3
Kristjan <radiojustchill@gmail.com>
2020-08-27 17:15:56 +0300

    Created PetType JPA services. Closes #42

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/AbstractJPAService.java
@@ -0,0 +1,40 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//Example of a better solution
+
+//public abstract class AbstractJpaService<T extends BaseEntity, R extends JpaRepository<T, Long>> implements CrudService<T> {
+//    protected R repository;
+//
+//    public AbstractJpaService(R repository) {
+//        this.repository = repository;
+//    }
+//
+//    @Override
+//    public Set<T> findAll() {
+//        return new HashSet<>(repository.findAll());
+//    }
+//
+//    @Override
+//    public T findById(Long id) {
+//        return repository.findById(id).orElse(null);
+//    }
+//
+//    @Override
+//    public T save(T object) {
+//        return repository.save(object);
+//    }
+//
+//    @Override
+//    public void delete(T object) {
+//        repository.delete(object);
+//    }
+//
+//    @Override
+//    public void deleteById(Long id) {
+//        repository.deleteById(id);
+//    }
+//}
